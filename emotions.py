93ab5719c7a69226fd32802a6ff33ea65ea2d278
Christian Tamayo <142272826+christiantomato@users.noreply.github.com>
2024-11-05 13:10:39 -0500

    Add files via upload

---
+++ emotions.py
@@ -0,0 +1,28 @@
+# add a comment here with your name, email, and student number
+# you can not add any import lines to this file
+EMOTIONS = ['anger', 'joy', 'fear', 'trust', 'sadness', 'anticipation']
+
+
+def clean_text(comment):
+    # add your code here and remove the pass keyword on the next line
+    pass
+
+
+def make_keyword_dict(keyword_file_name):
+    # add your code here and remove the pass keyword on the next line
+    pass
+
+
+def classify_comment_emotion(comment, keywords):
+    # add your code here and remove the pass keyword on the next line
+    pass
+
+
+def make_comments_list(filter_country, comments_file_name):
+    # add your code here and remove the pass keyword on the next line
+    pass
+
+
+def make_report(comment_list, keywords, report_filename):
+    # add your code here and remove the pass keyword on the next line
+    pass
---
+++ main.py
@@ -0,0 +1,24 @@
+# add a comment here with your name, email, and student number.
+# do not add any additional import lines to this file.
+
+import os.path
+from emotions import *
+
+VALID_COUNTRIES = ['bangladesh', 'brazil', 'canada', 'china', 'egypt',
+                   'france', 'germany', 'india', 'iran', 'japan', 'mexico',
+                   'nigeria', 'pakistan', 'russia', 'south korea', 'turkey',
+                   'united kingdom',  'united states']
+
+
+def ask_user_for_input():
+    # add your code here and remove the pass keyword on the next line
+    pass
+
+
+def main():
+    # add your code here and remove the pass keyword on the next line
+    pass
+
+
+if __name__ == "__main__":
+    main()
